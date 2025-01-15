9414cbff0a58b503672a062c332d8edfc00f9b97
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-01-15 19:23:55 -0300

    feat: add category sequelize repository

---
+++ src/shared/infra/db/sequelize/__tests__/category-model-mapper.spec.ts
@@ -0,0 +1,58 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Category } from '../../../../../category/domain/category.entity';
+import { Uuid } from '../../../../domain/value-objects/uuid.vo';
+import { CategoryModel } from '../category.model';
+import { CategoryModelMapper } from '../category-model-mapper';
+
+describe('CategoryModelMapper Integration Tests', () => {
+    let sequelize;
+    beforeEach(async () => {
+      sequelize = new Sequelize({ 
+          dialect: 'sqlite',
+          storage: ':memory:',
+          models: [CategoryModel],
+          logging: false,
+      })
+      await sequelize.sync();
+    })
+  it('should convert a category model to a category aggregate', () => {
+    const created_at = new Date();
+    const model = CategoryModel.build({
+      category_id: '5490020a-e866-4229-9adc-aa44b83234c4',
+      name: 'some value',
+      description: 'some description',
+      is_active: true,
+      created_at,
+    });
+    const aggregate = CategoryModelMapper.toEntity(model);
+    expect(aggregate.toJSON()).toStrictEqual(
+      new Category({
+        category_id: new Uuid('5490020a-e866-4229-9adc-aa44b83234c4'),
+        name: 'some value',
+        description: 'some description',
+        is_active: true,
+        created_at,
+      }).toJSON(),
+    );
+  });
+
+  it('should convert category entity to category model', () => {
+    const created_at = new Date();
+    const entity = Category
+        .fake()
+        .aCategory()
+        .withCategoryId(new Uuid('5490020a-e866-4229-9adc-aa44b83234c4'))
+        .withCreatedAt(created_at)
+        .withDescription('some description')
+        .withName('some value')
+        .build();
+    const model = CategoryModelMapper.toModel(entity);
+    expect(model.toJSON()).toStrictEqual({
+      category_id: '5490020a-e866-4229-9adc-aa44b83234c4',
+      name: 'some value',
+      description: 'some description',
+      is_active: true,
+      created_at,
+    });
+  })
+});
