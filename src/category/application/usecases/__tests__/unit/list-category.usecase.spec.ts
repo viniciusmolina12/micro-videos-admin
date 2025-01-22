8139d688a24b2575e413ddd156cc0f57ebd22b3a
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-01-22 08:58:09 -0300

    test: add list category usecase unit tests

---
+++ src/category/application/usecases/__tests__/unit/list-category.usecase.spec.ts
@@ -0,0 +1,131 @@
+import { CategoryInMemoryRepository } from "../../../../../shared/infra/db/in-memory/category/category-in-memory.repository";
+import { Category } from "../../../../domain/category.entity";
+import { CategorySearchResult } from "../../../../domain/category.repository";
+import { ListCategoryUseCase } from "../../list-category.usecase";
+import { CategoryOutputMapper } from "../../shared/category-output";
+
+describe('ListCategoriesUseCase Unit Tests', () => {
+  let useCase: ListCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new ListCategoryUseCase(repository);
+  });
+
+  test('toOutput method', () => {
+    let result = new CategorySearchResult({
+      items: [],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+    });
+    let output = useCase['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+      last_page: 1,
+    });
+
+    const entity = Category.create({ name: 'Movie' });
+    result = new CategorySearchResult({
+      items: [entity],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+    });
+
+    output = useCase['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [entity].map(CategoryOutputMapper.toOutput),
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+      last_page: 1,
+    });
+  });
+
+  it('should return output sorted by created_at when input param is empty', async () => {
+    const items = [
+      new Category({ name: 'test 1' }),
+      new Category({
+        name: 'test 2',
+        created_at: new Date(new Date().getTime() + 100),
+      }),
+    ];
+    repository.items = items;
+
+    const output = await useCase.execute({});
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map(CategoryOutputMapper.toOutput),
+      total: 2,
+      current_page: 1,
+      per_page: 15,
+      last_page: 1,
+    });
+  });
+
+  it('should return output using pagination, sort and filter', async () => {
+    const items = [
+      new Category({ name: 'a' }),
+      new Category({
+        name: 'AAA',
+      }),
+      new Category({
+        name: 'AaA',
+      }),
+      new Category({
+        name: 'b',
+      }),
+      new Category({
+        name: 'c',
+      }),
+    ];
+    repository.items = items;
+
+    let output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      filter: 'a',
+    });
+    expect(output).toStrictEqual({
+      items: [items[1], items[2]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 2,
+      per_page: 2,
+      sort: 'name',
+      filter: 'a',
+    });
+    expect(output).toStrictEqual({
+      items: [items[0]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 2,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc',
+      filter: 'a',
+    });
+    expect(output).toStrictEqual({
+      items: [items[0], items[2]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+  });
+});
