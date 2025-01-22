f2f95097085100138bbd9ca10307bcb59e506b9d
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-01-22 08:55:55 -0300

    feat: add pagination output

---
+++ src/shared/application/pagination-output.ts
@@ -0,0 +1,21 @@
+import { SearchResult } from "../domain/repository/search-result"
+
+export type PaginationOutput<Item = any> = {
+    items: Item[],
+    total: number,
+    current_page: number,
+    last_page: number,
+    per_page: number
+}
+
+export class PaginationOutputMapper {
+    static toOutput<Item>(items: Item[], props: SearchResult): PaginationOutput<Item> {
+        return {
+            items,
+            total: props.total,
+            current_page: props.current_page,
+            per_page: props.per_page,
+            last_page: props.last_page
+        }
+    }
+}
