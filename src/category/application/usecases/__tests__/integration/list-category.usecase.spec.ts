f66ddc2616d86228eebc96542c9be40b3feb2a55
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-01-22 08:57:57 -0300

    test: add list category usecase integration tests

---
+++ src/category/application/usecases/__tests__/integration/list-category.usecase.spec.ts
@@ -0,0 +1,101 @@
+import { CategorySequelizeRepository } from "../../../../../shared/infra/db/sequelize/category-sequelize.repository";
+import { CategoryModel } from "../../../../../shared/infra/db/sequelize/category.model";
+import { setupSequelize } from "../../../../../shared/infra/testing/helpers";
+import { Category } from "../../../../domain/category.entity";
+import { ListCategoryUseCase } from "../../list-category.usecase";
+import { CategoryOutputMapper } from "../../shared/category-output";
+
+describe('ListCategoriesUseCase Integration Tests', () => {
+  let useCase: ListCategoryUseCase;
+  let repository: CategorySequelizeRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new ListCategoryUseCase(repository);
+  });
+
+  it('should return output sorted by created_at when input param is empty', async () => {
+    const categories = Category.fake()
+      .theCategories(2)
+      .withCreatedAt((i) => new Date(new Date().getTime() + 1000 + i))
+      .build();
+
+    await repository.bulkInsert(categories);
+    const output = await useCase.execute({});
+    console.log('categories!!', categories)
+    console.log('output!!', output)
+    const teste = [...categories].reverse().map(CategoryOutputMapper.toOutput);
+    console.log('teste!!', teste)
+    expect(output).toEqual({
+      items: [...categories].reverse().map(CategoryOutputMapper.toOutput),
+      total: 2,
+      current_page: 1,
+      per_page: 15,
+      last_page: 1,
+    });
+  });
+
+  it('should returns output using pagination, sort and filter', async () => {
+    const categories = [
+      new Category({ name: 'a' }),
+      new Category({
+        name: 'AAA',
+      }),
+      new Category({
+        name: 'AaA',
+      }),
+      new Category({
+        name: 'b',
+      }),
+      new Category({
+        name: 'c',
+      }),
+    ];
+    await repository.bulkInsert(categories);
+
+    let output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      filter: 'a',
+    });
+    expect(output).toEqual({
+      items: [categories[1], categories[2]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 2,
+      per_page: 2,
+      sort: 'name',
+      filter: 'a',
+    });
+    expect(output).toEqual({
+      items: [categories[0]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 2,
+      per_page: 2,
+      last_page: 2,
+    });
+
+    output = await useCase.execute({
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc',
+      filter: 'a',
+    });
+    expect(output).toEqual({
+      items: [categories[0], categories[2]].map(CategoryOutputMapper.toOutput),
+      total: 3,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+    });
+  });
+});
