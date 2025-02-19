6979ca34e25597dc4e1ea653a4e0d837b6f64780
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-02-19 08:42:13 -0300

    feat: add database and categories module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
