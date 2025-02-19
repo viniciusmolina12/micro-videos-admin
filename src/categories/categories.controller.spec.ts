9b32a62312268f5ee37d0e4a2f79917ddf241d61
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-02-19 08:41:45 -0300

    feat: add category module

---
+++ src/categories/categories.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoriesController } from './categories.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { CategoriesModule } from './categories.module';
+
+describe('CategoriesController', () => {
+  let controller: CategoriesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, CategoriesModule],
+    }).compile();
+
+    controller = module.get<CategoriesController>(CategoriesController);
+  });
+
+  it('should be defined', () => {
+    console.log(controller);
+    expect(controller).toBeDefined();
+  });
+});
