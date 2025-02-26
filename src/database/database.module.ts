ba3ec72eec9cd5b6503d1e423709de1ff1107c11
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-02-26 08:58:28 -0300

    feat: add database module configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,42 @@
 import { CategoryModel } from '@core/shared/infra/db/sequelize/category.model';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { CONFIG_SCHEMA_TYPE } from 'src/config/config.module';
 
 const models = [CategoryModel];
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      host: ':memory:',
-      logging: false,
-      models,
-      autoLoadModels: true,
+    SequelizeModule.forRootAsync({
+      useFactory: async (configService: ConfigService<CONFIG_SCHEMA_TYPE>) => {
+        const dbVendor = configService.get('DB_VENDOR');
+        if (configService.get('DB_VENDOR') === 'sqlite') {
+          return {
+            dialect: 'sqlite',
+            models,
+            storage: configService.get('DB_DATABASE'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+          };
+        }
+
+        if (configService.get('DB_VENDOR') === 'mysql') {
+          return {
+            dialect: 'mysql',
+            models,
+            host: configService.get('DB_HOST'),
+            port: configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_DATABASE'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+          };
+        }
+
+        throw new Error(`Unsupported database config: ${dbVendor}`);
+      },
+      inject: [ConfigService],
     }),
   ],
 })
