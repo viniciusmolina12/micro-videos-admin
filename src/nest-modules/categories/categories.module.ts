e79cdb76246e283fc8adbda2bfc5b7e23af78025
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-04-14 08:20:06 -0300

    feat: add categories controller and tests

--- src/nest-modules/categories/categories.module.ts
+++ src/nest-modules/categories/categories.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
-import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { CategoryModel } from '@core/shared/infra/db/sequelize/category.model';
-import { CategorySequelizeRepository } from '@core/shared/infra/db/sequelize/category-sequelize.repository';
-import {CATEGORY_PROVIDERS } from './categories.providers';
+import { CATEGORY_PROVIDERS } from './categories.providers';
 
 @Module({
   imports: [SequelizeModule.forFeature([CategoryModel])],
   controllers: [CategoriesController],
-  providers: [...Object.values(CATEGORY_PROVIDERS.REPOSITORIES), ...Object.values(CATEGORY_PROVIDERS.USE_CASES)]
+  providers: [
+    ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORY_PROVIDERS.USE_CASES),
+  ],
 })
 export class CategoriesModule {}
