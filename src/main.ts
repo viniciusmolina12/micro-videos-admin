492b7a93fde1718e190a588bb55e9931229ed2a8
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-04-16 08:36:44 -0300

    feat: add wrapper interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { WrapperDataInterceptor } from './nest-modules/shared/interceptors/wrapper-data/wrapper-data.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    errorHttpStatusCode: 422,
-  }))
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new WrapperDataInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
