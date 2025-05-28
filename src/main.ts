4a33472cb0471841743a8cfc774bb98ea08db76a
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-05-28 08:59:59 -0300

    tests: e2e tests

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,13 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { WrapperDataInterceptor } from './nest-modules/shared/interceptors/wrapper-data/wrapper-data.interceptor';
-import { NotFoundErrorFilter } from './nest-modules/shared/not-found/not-found-error.filter';
-import { EntityValidationErrorFilter } from './nest-modules/shared/not-found/entity-validation-error.filter';
+import { NotFoundErrorFilter } from './nest-modules/shared/filters/not-found-error.filter';
+import { EntityValidationErrorFilter } from './nest-modules/shared/filters/entity-validation-error.filter';
+import { applyGlobalConfig } from './nest-modules/global-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      errorHttpStatusCode: 422,
-    }),
-  );
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalInterceptors(new WrapperDataInterceptor());
-  app.useGlobalFilters(
-    new NotFoundErrorFilter(),
-    new EntityValidationErrorFilter(),
-  );
-
+  applyGlobalConfig(app);
   await app.listen(3000);
 }
 bootstrap();
--- src/nest-modules/categories/categories.presenter.ts
+++ src/nest-modules/categories/categories.presenter.ts
@@ -7,8 +7,9 @@ export class CategoryPresenter {
   id: string;
   name: string;
   description: string | null;
+  is_active: boolean;
   @Transform(({ value }: { value: Date }) => {
-    value.toISOString();
+    return value.toISOString();
   })
   created_at: Date;
 
@@ -16,6 +17,7 @@ export class CategoryPresenter {
     this.id = output.id;
     this.name = output.name;
     this.description = output.description;
+    this.is_active = output.is_active;
     this.created_at = output.created_at;
   }
 }
