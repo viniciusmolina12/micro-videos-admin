3a587cbd52e2e0e45dfaed20ade54a41f14c830f
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-06-10 20:02:26 -0300

    test: add list category e2e tests

---
+++ test/categories/list-categories.e2e.spec.ts
@@ -0,0 +1,78 @@
+import request from 'supertest';
+import { instanceToPlain } from 'class-transformer';
+import { ICategoryRepository } from '@core/category/domain/category.repository';
+import { startApp } from 'src/nest-modules/shared/testing/helpers';
+import { ListCategoriesFixture } from 'src/nest-modules/categories/__tests__/testing/category-fixture';
+import * as CategoryProviders from 'src/nest-modules/categories/categories.providers';
+import { CategoriesController } from 'src/nest-modules/categories/categories.controller';
+import { CategoryOutputMapper } from '@core/category/application/usecases/@shared/category-output';
+describe('CategoriesController (e2e)', () => {
+  describe('/categories (GET)', () => {
+    describe('should return categories sorted by created_at when request query is empty', () => {
+      let categoryRepo: ICategoryRepository;
+      const nestApp = startApp();
+      const { entitiesMap, arrange } =
+        ListCategoriesFixture.arrangeIncrementedWithCreatedAt();
+
+      beforeEach(async () => {
+        categoryRepo = nestApp.app.get<ICategoryRepository>(
+          CategoryProviders.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+        );
+        await categoryRepo.bulkInsert(Object.values(entitiesMap));
+      });
+
+      test.each(arrange)(
+        'when query params is $send_data',
+        async ({ send_data, expected }) => {
+          const queryParams = new URLSearchParams(send_data as any).toString();
+          return request(nestApp.app.getHttpServer())
+            .get(`/categories/?${queryParams}`)
+            .expect(200)
+            .expect({
+              data: expected.entities.map((e) =>
+                instanceToPlain(
+                  CategoriesController.serialize(
+                    CategoryOutputMapper.toOutput(e),
+                  ),
+                ),
+              ),
+              meta: expected.meta,
+            });
+        },
+      );
+    });
+
+    describe('should return categories using paginate, filter and sort', () => {
+      let categoryRepo: ICategoryRepository;
+      const nestApp = startApp();
+      const { entitiesMap, arrange } = ListCategoriesFixture.arrangeUnsorted();
+
+      beforeEach(async () => {
+        categoryRepo = nestApp.app.get<ICategoryRepository>(
+          CategoryProviders.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+        );
+        await categoryRepo.bulkInsert(Object.values(entitiesMap));
+      });
+
+      test.each([arrange])(
+        'when query params is $send_data',
+        async ({ send_data, expected }) => {
+          const queryParams = new URLSearchParams(send_data as any).toString();
+          return request(nestApp.app.getHttpServer())
+            .get(`/categories/?${queryParams}`)
+            .expect(200)
+            .expect({
+              data: expected.entities.map((e) =>
+                instanceToPlain(
+                  CategoriesController.serialize(
+                    CategoryOutputMapper.toOutput(e),
+                  ),
+                ),
+              ),
+              meta: expected.meta,
+            });
+        },
+      );
+    });
+  });
+});
