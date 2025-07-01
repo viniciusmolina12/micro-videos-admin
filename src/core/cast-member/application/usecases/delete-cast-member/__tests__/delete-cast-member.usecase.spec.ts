2100a7878de6dd08344bf3e11d4a78fe17c63711
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-07-01 19:08:53 -0300

    test: add delete cast member usecase tests

---
+++ src/core/cast-member/application/usecases/delete-cast-member/__tests__/delete-cast-member.usecase.spec.ts
@@ -0,0 +1,34 @@
+import { CastMemberInMemoryRepository } from '@core/cast-member/infra/db/in-memory/cast-member-in-memory.repository';
+import { DeleteCastMemberUseCase } from '../delete-cast-member.usecase';
+import {
+  CastMember,
+  CastMemberId,
+} from '@core/cast-member/domain/cast-member.aggregate';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+
+describe('DeleteCastMemberUseCase Unit Tests', () => {
+  let useCase: DeleteCastMemberUseCase;
+  let repository: CastMemberInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CastMemberInMemoryRepository();
+    useCase = new DeleteCastMemberUseCase(repository);
+  });
+
+  it('should throws error when entity not found', async () => {
+    const castMemberId = new CastMemberId();
+
+    await expect(() =>
+      useCase.execute({ id: castMemberId.id }),
+    ).rejects.toThrow(new NotFoundError(castMemberId.id, CastMember));
+  });
+
+  it('should delete a cast member', async () => {
+    const items = [CastMember.fake().anActor().build()];
+    repository.items = items;
+    await useCase.execute({
+      id: items[0].cast_member_id.id,
+    });
+    expect(repository.items).toHaveLength(0);
+  });
+});
