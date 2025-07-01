f5037ab6bb9ceb03a98e93a3d5ba5363c83c67fe
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-07-01 09:26:40 -0300

    test: add create cast member tests

--- src/core/shared/infra/db/in-memory/in-memory.repository.spec.ts
+++ src/core/shared/infra/db/in-memory/in-memory.repository.spec.ts
@@ -1,6 +1,6 @@
-import { Entity } from '../../../domain/entity';
-import { NotFoundError } from '../../../domain/errors/not-found.error';
-import { Uuid } from '../../../domain/value-objects/uuid.vo';
+import { Entity } from '@core/shared/domain/entity';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+import { Uuid } from '@core/shared/domain/value-objects/uuid.vo';
 import { InMemoryRepository } from './in-memory.repository';
 
 type StubEntityConstructor = {
