d1c7f9f93132bb137d33903727025fd7b55bf040
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-07-01 09:15:10 -0300

    feat: add create cast member usecase

--- src/core/shared/infra/db/in-memory/in-memory.repository.ts
+++ src/core/shared/infra/db/in-memory/in-memory.repository.ts
@@ -1,21 +1,27 @@
-import { Entity } from '../../../domain/entity';
-import { NotFoundError } from '../../../domain/errors/not-found.error';
+import { Entity } from '@core/shared/domain/entity';
+import { InvalidArgumentError } from '@core/shared/domain/errors/invalid-argument.error';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
 import {
   IRepository,
   ISearchableRepository,
-} from '../../../domain/repository/repository-interface';
+} from '@core/shared/domain/repository/repository-interface';
 import {
   SearchParams,
   SortDirection,
-} from '../../../domain/repository/search-params';
-import { SearchResult } from '../../../domain/repository/search-result';
-import { ValueObject } from '../../../domain/value-object';
+} from '@core/shared/domain/repository/search-params';
+import { SearchResult } from '@core/shared/domain/repository/search-result';
+import { ValueObject } from '@core/shared/domain/value-object';
 
 export abstract class InMemoryRepository<
   E extends Entity,
   EntityId extends ValueObject,
 > implements IRepository<E, EntityId>
 {
+  existsById(
+    ids: EntityId[],
+  ): Promise<{ exists: EntityId[]; not_exists: EntityId[] }> {
+    throw new Error('Method not implemented.');
+  }
   items: E[] = [];
   async insert(entity: E): Promise<void> {
     this.items.push(entity);
@@ -82,6 +88,34 @@ export abstract class InMemorySearchableRepository<
       current_page: props.page,
       per_page: props.per_page,
     });
+  }
+
+  async existsById(
+    ids: EntityId[],
+  ): Promise<{ exists: EntityId[]; not_exists: EntityId[] }> {
+    if (!ids.length) {
+      throw new InvalidArgumentError(
+        'ids must be an array with at least one element',
+      );
+    }
+
+    if (this.items.length === 0) {
+      return {
+        exists: [],
+        not_exists: ids,
+      };
+    }
+
+    const existsId = new Set<EntityId>();
+    const notExistsId = new Set<EntityId>();
+    ids.forEach((id) => {
+      const item = this.items.find((entity) => entity.entity_id.equals(id));
+      item ? existsId.add(id) : notExistsId.add(id);
+    });
+    return {
+      exists: Array.from(existsId.values()),
+      not_exists: Array.from(notExistsId.values()),
+    };
   }
 
   protected abstract applyFilter(
