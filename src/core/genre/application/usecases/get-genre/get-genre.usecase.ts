14a8c79c0eb91ff573ccff46f5038b404f8ce662
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-08-12 07:46:33 -0300

    feat: add get genre usecase

---
+++ src/core/genre/application/usecases/get-genre/get-genre.usecase.ts
@@ -0,0 +1,33 @@
+import { ICategoryRepository } from '@core/category/domain/category.repository';
+import { IUseCase } from '@core/shared/application/use-case.interface';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+import { Genre, GenreId } from '@core/genre/domain/genre.aggregate';
+import { IGenreRepository } from '@core/genre/domain/genre.repository';
+import { GenreOutput, GenreOutputMapper } from '../common/genre-output';
+
+export class GetGenreUseCase
+  implements IUseCase<GetGenreInput, GetGenreOutput>
+{
+  constructor(
+    private genreRepo: IGenreRepository,
+    private categoryRepo: ICategoryRepository,
+  ) {}
+
+  async execute(input: GetGenreInput): Promise<GetGenreOutput> {
+    const genreId = new GenreId(input.id);
+    const genre = await this.genreRepo.findById(genreId);
+    if (!genre) {
+      throw new NotFoundError(input.id, Genre);
+    }
+    const categories = await this.categoryRepo.findByIds([
+      ...genre.categories_id.values(),
+    ]);
+    return GenreOutputMapper.toOutput(genre, categories);
+  }
+}
+
+export type GetGenreInput = {
+  id: string;
+};
+
+export type GetGenreOutput = GenreOutput;
