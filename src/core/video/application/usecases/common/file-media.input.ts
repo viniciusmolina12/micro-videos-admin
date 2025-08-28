bcff37b2a2ea28608e6e1f472be026c7e3f06549
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-08-28 07:53:24 -0300

    feat: add file media and upload image medias input interfaces

---
+++ src/core/video/application/usecases/common/file-media.input.ts
@@ -0,0 +1,34 @@
+import { IsInstance, IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export type FileMediaInputContructorProps = {
+  raw_name: string;
+  data: Buffer;
+  mime_type: string;
+  size: number;
+};
+
+export class FileMediaInput {
+  @IsString()
+  @IsNotEmpty()
+  raw_name: string;
+
+  @IsInstance(Buffer)
+  @IsNotEmpty()
+  data: Buffer;
+
+  @IsString()
+  @IsNotEmpty()
+  mime_type: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  size: number;
+
+  constructor(props: FileMediaInputContructorProps) {
+    if (!props) return;
+    this.raw_name = props.raw_name;
+    this.data = props.data;
+    this.mime_type = props.mime_type;
+    this.size = props.size;
+  }
+}
