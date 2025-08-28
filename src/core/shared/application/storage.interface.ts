ad5a94af29ea25d77de67721586e924845eac171
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-08-28 07:52:27 -0300

    feat: add in memory storage

---
+++ src/core/shared/application/storage.interface.ts
@@ -0,0 +1,8 @@
+export interface IStorage {
+  store(object: {
+    data: Buffer;
+    mime_type?: string;
+    id: string;
+  }): Promise<void>;
+  get(id: string): Promise<{ data: Buffer; mime_type?: string }>;
+}
