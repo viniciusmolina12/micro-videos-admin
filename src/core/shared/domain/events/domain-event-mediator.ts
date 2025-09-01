0022e64a157835fd39e7e917a267fac81d429ffe
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-09-01 08:15:12 -0300

    feat: add domain event mediator

---
+++ src/core/shared/domain/events/domain-event-mediator.ts
@@ -0,0 +1,20 @@
+import EventEmitter2 from 'eventemitter2';
+import { AggregateRoot } from '../aggregate-root';
+
+export class DomainEventMediator {
+  constructor(private readonly eventEmitter: EventEmitter2) {
+    this.eventEmitter = new EventEmitter2();
+  }
+
+  register(event: string, handler: (event: any) => void) {
+    this.eventEmitter.on(event, handler);
+  }
+
+  async publish(aggregateRoot: AggregateRoot) {
+    aggregateRoot.getUncommittedEvents().forEach(async (event) => {
+      aggregateRoot.markEventAsDispatched(event);
+      await this.eventEmitter.emitAsync(event.constructor.name, event);
+    });
+    aggregateRoot.events.clear();
+  }
+}
