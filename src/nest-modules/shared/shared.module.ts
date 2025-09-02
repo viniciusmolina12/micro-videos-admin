7f67ca6a8f84f5f4d5d96939d71016f42333e11e
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-09-02 10:15:17 -0300

    feat: add google credentials on config module

--- src/nest-modules/shared/shared.module.ts
+++ src/nest-modules/shared/shared.module.ts
@@ -1,7 +1,43 @@
-import { Module } from '@nestjs/common';
-import { CollectionPresenter } from './collection.presenter';
+import { Global, Module, Scope } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GoogleCloudStorage } from '@core/shared/infra/storage/google-cloud/google-cloud.storage';
+import { Storage as GoogleCloudStorageSdk } from '@google-cloud/storage';
+import { DomainEventMediator } from '@core/shared/domain/events/domain-event-mediator';
+import EventEmitter2 from 'eventemitter2';
+import { ApplicationService } from '@core/shared/application/application.service';
+import { IUnitOfWork } from '@core/shared/domain/repository/unit-of-work.interface';
 
+@Global()
 @Module({
-  exports: [],
+  providers: [
+    {
+      provide: 'IStorage',
+      useFactory: (configService: ConfigService) => {
+        const credentials = configService.get('GOOGLE_CLOUD_CREDENTIALS');
+        const bucket = configService.get('GOOGLE_CLOUD_STORAGE_BUCKET_NAME');
+        const storage = new GoogleCloudStorageSdk({
+          credentials,
+        });
+        return new GoogleCloudStorage(storage, bucket);
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: DomainEventMediator,
+      useValue: new DomainEventMediator(new EventEmitter2()),
+    },
+    {
+      provide: ApplicationService,
+      useFactory: (
+        domainEventMediator: DomainEventMediator,
+        uow: IUnitOfWork,
+      ) => {
+        return new ApplicationService(uow, domainEventMediator);
+      },
+      inject: ['UnitOfWork', DomainEventMediator],
+      scope: Scope.REQUEST,
+    },
+  ],
+  exports: ['IStorage', ApplicationService],
 })
 export class SharedModule {}
