cf97980ffdd23d1e1a02f46e4b62713922844ea5
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-09-18 08:15:42 -0300

    feat: add rabbitmq message broker

---
+++ src/core/shared/infra/message-broker/rabbitmq/rabbitmq-message-broker.ts
@@ -0,0 +1,13 @@
+import { IMessageBroker } from '@core/shared/application/message-broker.interface';
+import { IDomainEvent } from '@core/shared/domain/events/domain-event.interface';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { EVENTS_MESSAGE_BROKER_CONFIG } from './events-message-broker-config';
+
+export class RabbitMQMessageBroker implements IMessageBroker {
+  constructor(private readonly connection: AmqpConnection) {}
+
+  async publishEvent(event: IDomainEvent): Promise<void> {
+    const config = EVENTS_MESSAGE_BROKER_CONFIG[event.constructor.name];
+    await this.connection.publish(config.exchange, config.routingKey, event);
+  }
+}
