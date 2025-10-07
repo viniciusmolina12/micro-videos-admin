d933a0e91a9397234e1fb29c949045c847c93df8
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-10-07 20:28:45 -0300

    feat: add rabbitmq filter on module

--- src/nest-modules/rabbitmq/rabbitmq.module.ts
+++ src/nest-modules/rabbitmq/rabbitmq.module.ts
@@ -6,6 +6,7 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CONFIG_SCHEMA_TYPE } from '../config/config.module';
 import { RabbitMQMessageBroker } from '@core/shared/infra/message-broker/rabbitmq/rabbitmq-message-broker';
+import { RabbitmqConsumeErrorFilter } from './rabbitmq-consume-error/rabbitmq-consume-error.filter';
 
 export class RabbitmqModule {
   static forRoot(): DynamicModule {
@@ -16,12 +17,35 @@ export class RabbitmqModule {
         GoLevelUpRabbitMQModule.forRootAsync({
           useFactory: (configService: ConfigService<CONFIG_SCHEMA_TYPE>) => {
             return {
-              uri: configService.get('RABBITMQ_URI'),
+              uri: configService.get('RABBITMQ_URI') as string,
+              exchanges: [
+                {
+                  name: 'dlx.exchange',
+                  type: 'topic',
+                },
+                {
+                  name: 'direct.delayed',
+                  type: 'x-delayed-message',
+                  options: {
+                    arguments: {
+                      'x-delayed-type': 'direct',
+                    },
+                  },
+                },
+              ],
+              queues: [
+                {
+                  name: 'dlx.queue',
+                  exchange: 'dlx.exchange',
+                  routingKey: '#',
+                },
+              ],
             };
           },
           inject: [ConfigService],
         }),
       ],
+      providers: [RabbitmqConsumeErrorFilter],
       exports: [GoLevelUpRabbitMQModule],
     };
   }
